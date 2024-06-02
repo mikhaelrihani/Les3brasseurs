7e1f1142f493da81408b837e3aa42323b6dbec4b
mika rihani <mikabernik@gamil.com>
2024-06-02 12:02:29 +0400

    feat(app): sending message with twillio

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -30,3 +30,7 @@ class SecurityController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
+<<<<<<< HEAD
+=======
+
+>>>>>>> 22262f8 (feat(app): sending message with twillio)
